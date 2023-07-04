65c9ff78c2c398de1d1e904064fbc0959fe4ec01
maxchristensen <you@example.com>
2023-07-04 12:29:22 +1200

    single project created.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,9 +4,42 @@ import { useParams } from 'react-router-dom'
 
 const Project = () => {
     const [project, setProjects] = useState([])
-    const [loading, setLoading] = useState(true)
 
     const {id} = useParams()
+
+    useEffect(() => {
+        axios.get(`${endpoint}`)
+        .then((res) => {
+          console.log(res)
+          setPost(res.data)
+          setLoading(false)
+        })
+        .catch((err) => console.log(err))
+      }, [id])
+    
+      return (
+        <div className='single-project-container' key={project.id}>
+                <div className="single-project-tab">
+                <div className="single-project-image">
+                    <img src={project.image[0].image_link}/>
+                    </div>
+                <div className="single-project-image">
+                    <img src={project.image[1].image_link}/>
+                </div>
+                </div>
+                <div className="single-project-tab">
+                <div className="single-project-image">
+                    <img src={project.image[2].image_link}/>
+                    </div>
+                <div className="single-project-image">
+                    <img src={project.image[3].image_link}/>
+                </div>
+                </div>
+                <div className="single-project-2-tabs">
+
+                </div>
+        </div>
+     );
 
 
   return (
@@ -30,7 +63,6 @@ const Project = () => {
         <div className="single-project-2-tabs">
 
         </div>
-        
     </div>
   )
 }
