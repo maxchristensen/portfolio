daf1c5bc809a5f9c7441e3251ae4d4af5c33dcee
maxchristensen <you@example.com>
2023-07-04 12:49:23 +1200

    deleted second return

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -42,31 +42,6 @@ const Project = () => {
                 </div>
         </div>
      );
-
-
-  return (
-    <div className='single-project-container'>
-        <div className="single-project-tab">
-        <div className="single-project-image">
-              <img src={project.image[0].image_link}/>
-            </div>
-        <div className="single-project-image">
-            <img src={project.image[1].image_link}/>
-        </div>
-        </div>
-        <div className="single-project-tab">
-        <div className="single-project-image">
-              <img src={project.image[2].image_link}/>
-            </div>
-        <div className="single-project-image">
-            <img src={project.image[3].image_link}/>
-        </div>
-        </div>
-        <div className="single-project-2-tabs">
-
-        </div>
-    </div>
-  )
 }
 
 export default Project
